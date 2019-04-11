46038e89bdeb9773d4f6e5d5b2eadc82789ab1ef
Gabriel Olmos <gabe_olmos@yahoo.com>
2019-04-11 14:57:32 -0700

    initial commit, some html and js sample of Blockchain

---
+++ main.js
@@ -0,0 +1,12 @@
+class Block {
+    constructor (index, timestamp, date, previousHash = '' ){
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = '';
+    }
+    calculateHaash(){
+        
+    }
+}
